8497a98a1cd75bf729509d768802eb2969aac693
inferno333 <inferno1968@proton.me>
2022-09-09 14:25:52 +0000

    port to new api

--- src/alertor.rs
+++ src/alertor.rs
@@ -2,3 +2,5 @@ use dotenv::dotenv;
 use lettre::{smtp::authentication::Credentials, SmtpClient, SmtpTransport, Transport};
 use lettre_email::EmailBuilder;
 use std::process::Command;
+
+use crate::error::{Erro
