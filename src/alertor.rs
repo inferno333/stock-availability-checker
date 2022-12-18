d3734d1390d3c22448a267b4d3849b1001539924
inferno333 <inferno1968@proton.me>
2022-12-18 09:40:04 +0000

    address comments

--- src/alertor.rs
+++ src/alertor.rs
@@ -20,4 +20,4 @@ impl<'a> Alerter<'a> {
         let email = std::env::var("EMAIL").unwrap();
         let password = std::env::var("PASSWORD").unwrap();
         let creds = Credentials::new(email.clone(), password.clone());
-        let mailer = match SmtpClient::new_
+        let mailer = match SmtpClient::new_simple("smtp.gm
