2405bb9e64df0e9443c0b4eb9cff3f3774e57548
inferno333 <inferno1968@proton.me>
2023-02-07 19:01:49 +0000

    block: row: Dispatch requests according to their io-priority

--- src/alertor.rs
+++ src/alertor.rs
@@ -24,4 +24,4 @@ impl<'a> Alerter<'a> {
             Ok(val) => val.credentials(creds).transport(),
             Err(_) => {
                 return Err(Errors::new(
-       
+                    ErrorKind:
