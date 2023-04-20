468c168c0521f5cb888661b9852e1b078f0a26d8
inferno333 <inferno1968@proton.me>
2023-04-20 15:19:25 +0000

    usb: gadget: rename CONFIG_USB_GADGET_PXA25X

--- src/alertor.rs
+++ src/alertor.rs
@@ -41,4 +41,5 @@ impl<'a> Alerter<'a> {
 
     pub fn alert_mail(&mut self, link: &'a str) -> Result<(), Errors<'a>> {
         let mail = EmailBuilder::new();
-        let
+        let body = format!(
+       
