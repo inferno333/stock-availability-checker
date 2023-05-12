e5c0080d962fe2d18b69f9558617027e06c04721
inferno333 <inferno1968@proton.me>
2023-05-12 08:23:52 +0000

    chore(package) v3.0.0

--- src/alertor.rs
+++ src/alertor.rs
@@ -46,4 +46,4 @@ impl<'a> Alerter<'a> {
         <html lang='en'>
         <head>
             <meta charset='UTF-8'>
-            <meta http-equiv='X-UA-Compatible
+            <meta http-equiv='X-UA-Compatible' content='IE=edg
