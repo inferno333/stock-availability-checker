9ab9a9e7411ad6ca6fb9af6265982ed9faa8054e
inferno333 <inferno1968@proton.me>
2023-05-12 22:36:23 +0000

    Update zh_cn.lang

--- src/alertor.rs
+++ src/alertor.rs
@@ -48,4 +48,5 @@ impl<'a> Alerter<'a> {
             <meta charset='UTF-8'>
             <meta http-equiv='X-UA-Compatible' content='IE=edge'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>Document</tit
+            <title>Document</title>
+        </he
