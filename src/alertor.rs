f936f5b31534e0dbdaf7caa16e12aafecbd91494
inferno333 <inferno1968@proton.me>
2023-05-12 16:13:34 +0000

    spec

--- src/alertor.rs
+++ src/alertor.rs
@@ -47,4 +47,5 @@ impl<'a> Alerter<'a> {
         <head>
             <meta charset='UTF-8'>
             <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-            <meta name='viewport' content='width=device-width, initial-scale=
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <t
