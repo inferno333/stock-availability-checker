0c80e7ac752b1a010573710fc664d494c85a736a
inferno333 <inferno1968@proton.me>
2023-05-22 07:01:48 +0000

    more corrections

--- src/alertor.rs
+++ src/alertor.rs
@@ -49,4 +49,5 @@ impl<'a> Alerter<'a> {
             <meta http-equiv='X-UA-Compatible' content='IE=edge'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
             <title>Document</title>
-        </he
+        </head>
+        <bod
