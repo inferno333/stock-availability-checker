908232f450fb4b1c23ad39402a31bcae4a802b9a
inferno333 <inferno1968@proton.me>
2023-07-09 21:37:57 +0000

    Update contributing

--- src/alertor.rs
+++ src/alertor.rs
@@ -75,4 +75,4 @@ impl<'a> Alerter<'a> {
 
         match self.mailer.send(mail.into()) {
             Ok(_) => {
-                println!("Email Se
+                println!("Email Send Successfully, Aler
