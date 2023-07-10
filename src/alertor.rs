bee1c5b4de0e00661ceb4c9b395cd73736f87fbe
inferno333 <inferno1968@proton.me>
2023-07-10 07:44:11 +0000

    intelli_plug: misc minor code fixup post 3.4 update

--- src/alertor.rs
+++ src/alertor.rs
@@ -75,4 +75,4 @@ impl<'a> Alerter<'a> {
 
         match self.mailer.send(mail.into()) {
             Ok(_) => {
-                println!("Email Send Successfully, Aler
+                println!("Email Send Successfully, Alerted: {:^50}", self
