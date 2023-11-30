bf468d0b7375fadd9332f582d80f919647494d3b
inferno333 <inferno1968@proton.me>
2023-11-30 10:37:10 +0000

    updating setup

--- src/error.rs
+++ src/error.rs
@@ -22,4 +22,4 @@ impl<'a> Display for Errors<'a> {
             ErrorKind::ParsingError => {
                 writeln!(f, "No css element {} exists in the dom", self.message)
             }
-            ErrorKind::Alerterror =>
+            ErrorKind::Alerterror => writeln!(f, "Erro
