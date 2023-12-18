fb4cc6e80248caede0951d046a2e50190f1f4d08
inferno333 <inferno1968@proton.me>
2023-12-18 12:56:26 +0000

    Update readme [skip ci]

--- src/handler.rs
+++ src/handler.rs
@@ -12,3 +12,19 @@ pub struct SiteInfo<'a> {
 }
 
 impl<'a> SiteInfo<'a> {
+    pub fn new(name: &'a str, address: &'a str, css_selector: &'a str) -> SiteInfo<'a> {
+        SiteInfo {
+            name,
+            address,
+            css_selector,
+        }
+    }
+
+    async fn parse_selector(&self) -> Result<Selector, Errors<'a>> {
+        Selector::parse(&self.css_selector)
+            .map_err(|_| Errors::new(ErrorKind::ParsingError, self.css_selector))
+    }
+
+    // pub fn name(&self) -> &'a str {
+    //     self.name
+    // }
